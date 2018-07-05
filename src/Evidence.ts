976a60e2dacb119313d00922a46787fb488cd33e
Aleix Morgadas <aleix602@gmail.com>
2018-07-05 23:39:14 +0200

    CREATED Evidence class to refer to the command logic that was applied

---
+++ src/Evidence.ts
@@ -0,0 +1,4 @@
+export class Evidence {
+    constructor(public readonly block: number,
+                public readonly transaction: string) {}
+}
---
+++ test/Evidence.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { Evidence } from '../src/Evidence';
+
+describe('Evidence', () => {
+    it('should contain the block and the transaction hash', () => {
+        const evidence = new Evidence(500, '62D7F806C7E7327DB55F4FBB2348DD7785452FD55EEC60BB5D9672AF81141960');
+        expect(evidence.block).to.be.equal(500);
+        expect(evidence.transaction)
+            .to.be.equal('62D7F806C7E7327DB55F4FBB2348DD7785452FD55EEC60BB5D9672AF81141960');
+    });
+});
